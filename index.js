316c592c2f55a4bf5429e4b5e62e18ab8090567e
zpoettker <poettkerzach@gmail.com>
2024-08-05 23:32:01 +0000

    Fixed buttons, added placement functions

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ let fighting;
 let monsterHealth;
 let inventory = ["stick"];
 
-const button1 = document.querySelector(".button1");
-const button2 = document.querySelector(".button2");
-const button3 = document.querySelector(".button3");
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
 
 const text = document.querySelector(".text");
 const foodText = document.querySelector(".foodText");
@@ -29,7 +29,7 @@ const locations = [
     {
       name: "town square",
       "button text": ["Go to store", "Go to cave", "Go to odd job"],
-      "button functions": [goStore, goCave, oddJob],
+      "button functions": [goStore, goCave, goTown],
       text: "You are in the town square. You see a sign that says \"Store\"."
     },
     {
@@ -45,6 +45,11 @@ const locations = [
       text: "You enter the cave. You see some monsters."
     }
   ];
+
+  // initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
 
  function update(location){
   button1.innerText = location["button text"][0];
@@ -68,4 +73,20 @@ function goCave() {
   update(locations[2]);
 }
 
-  
+//paceholder functions
+function fightSlime(){
+  console.log("hello");
+}
+function fightDragon(){
+  console.log("hello");
+}
+function buyHealth(){
+  console.log("hello");
+}
+function buyWeapon(){
+  console.log("hello");
+}
+
+function fightBeast(){
+  console.log("hello");
+}
